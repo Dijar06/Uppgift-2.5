25f9690ba2b6e7ac28dc8ec84ae88850a4d570a5
Dijar06 <dize06001@utb.vaxjo.se>
2023-09-15 13:28:15 +0200

    Add project files.

---
+++ Uppgift 2.5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Uppgift2._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv en beräkning som innehåller två decimaltal och ett gångertecken imellan");
+            string beräkning = Console.ReadLine();
+            int gångerindex = beräkning.IndexOf("*");
+            string decimal1text = beräkning[..gångerindex];
+            string decimal2text = beräkning[(gångerindex + 1)..];
+            float decimal1 = float.Parse(decimal1text);
+            float decimal2 = float.Parse(decimal2text);
+            float svar = decimal1 * decimal2;
+            Console.WriteLine("Svaret är " + svar);
+        }
+    }
+}
